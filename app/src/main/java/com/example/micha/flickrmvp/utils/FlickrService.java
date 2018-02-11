e80c04ea1dbf93b9a05e152e3f1152072b597616
Mazogu <michael_doom@hotmail.com>
2018-02-11 01:58:18 -0500

    Added retrofit calls

--- app/src/main/java/com/example/micha/flickrmvp/utils/FlickrService.java
+++ app/src/main/java/com/example/micha/flickrmvp/utils/FlickrService.java
@@ -2,15 +2,25 @@ package com.example.micha.flickrmvp.utils;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class FlickrService extends Service {
+
+    FlickrBinder binder = new FlickrBinder();
+
     public FlickrService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    public class FlickrBinder extends Binder{
+        public FlickrService getService(){
+            return FlickrService.this;
+        }
     }
 }
--- app/src/main/java/com/example/micha/flickrmvp/utils/RetrofitHelper.java
+++ app/src/main/java/com/example/micha/flickrmvp/utils/RetrofitHelper.java
@@ -1,8 +1,16 @@
 package com.example.micha.flickrmvp.utils;
 
+
+
+import com.example.micha.flickrmvp.model.Photo;
+import com.example.micha.flickrmvp.model.PhotoList;
+
+import io.reactivex.Observable;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by micha on 2/10/2018.
@@ -15,8 +23,22 @@ public class RetrofitHelper {
                     .addConverterFactory(GsonConverterFactory.create()).baseUrl(url).build();
         }
 
-        public interface RetroFitService{
+        public static Observable<PhotoList> getPhotoList(String url, String user, String key){
+            RetroFitService service = Factory.getRetrofit(url).create(RetroFitService.class);
+            return service.getPhotoList(key,user);
+        }
 
+        public static Observable<Photo> getPhoto(String url,String key, String photoId){
+            RetroFitService service = Factory.getRetrofit(url).create(RetroFitService.class);
+            return service.getPhoto(key, photoId);
+        }
+
+        public interface RetroFitService{
+            @GET("rest/?method=flickr.people.getPublicPhotos&api_key={key}&user_id={user}&format=json")
+            Observable<PhotoList> getPhotoList(@Path("key") String key,@Path("user") String user);
+
+            @GET("rest/?method=flickr.photos.getInfo&api_key={key}&photo_id={photo_id}&format=json")
+            Observable<Photo> getPhoto(@Path("key") String key, @Path("photo_id") String photoId);
         }
     }
 }
